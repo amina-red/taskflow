568c185459926822d15f971f29eadf546bbb20bc
amina-red <amina.redjel@univ-constantine2.dz>
2026-07-06 16:28:02 +0100

    update cors with vercel url

--- backend/main.go
+++ backend/main.go
@@ -22,7 +22,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://taskflow-amina.vercel.app"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://taskflow-eosin-two.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
